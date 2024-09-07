a4d8895c4a6ba42f4f8dfcec15cab22cd8b0b4a8
erik <erikhervall11@gmail.com>
2024-09-07 16:48:04 -0700

    migration edit

--- migrations/env.py
+++ migrations/env.py
@@ -3,9 +3,14 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
-from flask import current_app
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -30,9 +35,16 @@ def get_engine():
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+# config.set_main_option(
+#     'sqlalchemy.url', str(get_engine().url).replace('%', '%%'))
+# target_db = current_app.extensions['migrate'].db
+
+from flask import current_app
+
 config.set_main_option(
-    'sqlalchemy.url', str(get_engine().url).replace('%', '%%'))
-target_db = current_app.extensions['migrate'].db
+    'sqlalchemy.url',
+    str(current_app.extensions['migrate'].db.engine.url).replace('%', '%%'))
+target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -40,10 +52,10 @@ target_db = current_app.extensions['migrate'].db
 # ... etc.
 
 
-def get_metadata():
-    if hasattr(target_db, 'metadatas'):
-        return target_db.metadatas[None]
-    return target_db.metadata
+# def get_metadata():
+#     if hasattr(target_db, 'metadatas'):
+#         return target_db.metadatas[None]
+#     return target_db.metadata
 
 
 def run_migrations_offline():
@@ -85,7 +97,11 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = get_engine()
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
@@ -94,8 +110,14 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
+        
+        # Create a schema (only in production)
+        if environment == "production":
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
         with context.begin_transaction():
+            if environment == "production":
+                context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
 
