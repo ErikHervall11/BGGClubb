6a8789f0c383c46e110b7028ea7e0dda5be7c7d1
erik <erikhervall11@gmail.com>
2024-09-08 17:31:55 -0700

    seed rounds gone

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,7 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .players import seed_players, undo_players
-from .rounds import seed_rounds, undo_rounds
+# from .rounds import seed_rounds, undo_rounds
 from .seed import seed_settings, undo_settings
 
 from app.models.db import db, environment, SCHEMA
@@ -21,11 +21,11 @@ def seed():
         # Make sure to add all your other model's undo functions below
         undo_users()
         undo_players()
-        undo_rounds()
+        # undo_rounds()
         undo_settings()
     seed_users()
     seed_players()
-    seed_rounds()
+    # seed_rounds()
     seed_settings()
     # Add other seed functions here
 
@@ -34,7 +34,7 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_settings()
-    undo_rounds()
+    # undo_rounds()
     undo_players()
     undo_users()
     # Add other undo functions here
--- app/seeds/rounds.py
+++ app/seeds/rounds.py
@@ -1,47 +1,47 @@
-from app.models import db, Round, Score, environment, SCHEMA
-from sqlalchemy.sql import text
+# from app.models import db, Round, Score, environment, SCHEMA
+# from sqlalchemy.sql import text
 
-def seed_rounds():
-    round1 = Round(scorer_id=1, attester_id=2, scorecard_image='https://bad-girls-golf-club.s3.us-west-1.amazonaws.com/McginnisGolfCourseScorecard1.png')
-    round2 = Round(scorer_id=2, attester_id=3, scorecard_image='https://bad-girls-golf-club.s3.us-west-1.amazonaws.com/McginnisGolfCourseScorecard1.png')
+# def seed_rounds():
+#     round1 = Round(scorer_id=1, attester_id=2, scorecard_image='https://bad-girls-golf-club.s3.us-west-1.amazonaws.com/McginnisGolfCourseScorecard1.png')
+#     round2 = Round(scorer_id=2, attester_id=3, scorecard_image='https://bad-girls-golf-club.s3.us-west-1.amazonaws.com/McginnisGolfCourseScorecard1.png')
 
-    scores = [
-        Score(round_id=1, player_id=1, hole_number=1, strokes=4),
-        Score(round_id=1, player_id=1, hole_number=2, strokes=5),
-        Score(round_id=1, player_id=1, hole_number=3, strokes=3),
-        Score(round_id=1, player_id=1, hole_number=4, strokes=3),
-        Score(round_id=1, player_id=1, hole_number=5, strokes=6),
-        Score(round_id=1, player_id=1, hole_number=6, strokes=3),
-        Score(round_id=1, player_id=1, hole_number=7, strokes=4),
-        Score(round_id=1, player_id=1, hole_number=8, strokes=5),
-        Score(round_id=1, player_id=1, hole_number=9, strokes=5),
+#     scores = [
+#         Score(round_id=1, player_id=1, hole_number=1, strokes=4),
+#         Score(round_id=1, player_id=1, hole_number=2, strokes=5),
+#         Score(round_id=1, player_id=1, hole_number=3, strokes=3),
+#         Score(round_id=1, player_id=1, hole_number=4, strokes=3),
+#         Score(round_id=1, player_id=1, hole_number=5, strokes=6),
+#         Score(round_id=1, player_id=1, hole_number=6, strokes=3),
+#         Score(round_id=1, player_id=1, hole_number=7, strokes=4),
+#         Score(round_id=1, player_id=1, hole_number=8, strokes=5),
+#         Score(round_id=1, player_id=1, hole_number=9, strokes=5),
         
         
 
-        # Add more scores for round1
-        Score(round_id=2, player_id=2, hole_number=1, strokes=3),
-        Score(round_id=2, player_id=2, hole_number=2, strokes=4),
-        Score(round_id=2, player_id=2, hole_number=3, strokes=3),
-        Score(round_id=2, player_id=2, hole_number=4, strokes=4),
-        Score(round_id=2, player_id=2, hole_number=5, strokes=4),
-        Score(round_id=2, player_id=2, hole_number=6, strokes=6),
-        Score(round_id=2, player_id=2, hole_number=7, strokes=4),
-        Score(round_id=2, player_id=2, hole_number=8, strokes=3),
-        Score(round_id=2, player_id=2, hole_number=9, strokes=3),
-        # Add more scores for round2
-    ]
+#         # Add more scores for round1
+#         Score(round_id=2, player_id=2, hole_number=1, strokes=3),
+#         Score(round_id=2, player_id=2, hole_number=2, strokes=4),
+#         Score(round_id=2, player_id=2, hole_number=3, strokes=3),
+#         Score(round_id=2, player_id=2, hole_number=4, strokes=4),
+#         Score(round_id=2, player_id=2, hole_number=5, strokes=4),
+#         Score(round_id=2, player_id=2, hole_number=6, strokes=6),
+#         Score(round_id=2, player_id=2, hole_number=7, strokes=4),
+#         Score(round_id=2, player_id=2, hole_number=8, strokes=3),
+#         Score(round_id=2, player_id=2, hole_number=9, strokes=3),
+#         # Add more scores for round2
+#     ]
 
-    db.session.add(round1)
-    db.session.add(round2)
-    db.session.add_all(scores)
+#     db.session.add(round1)
+#     db.session.add(round2)
+#     db.session.add_all(scores)
 
-    db.session.commit()
+#     db.session.commit()
 
-def undo_rounds():
-    if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.rounds RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.scores RESTART IDENTITY CASCADE;")
-    else:
-        db.session.execute("DELETE FROM rounds;")
-        db.session.execute("DELETE FROM scores;")
-    db.session.commit()
+# def undo_rounds():
+#     if environment == "production":
+#         db.session.execute(f"TRUNCATE table {SCHEMA}.rounds RESTART IDENTITY CASCADE;")
+#         db.session.execute(f"TRUNCATE table {SCHEMA}.scores RESTART IDENTITY CASCADE;")
+#     else:
+#         db.session.execute("DELETE FROM rounds;")
+#         db.session.execute("DELETE FROM scores;")
+#     db.session.commit()
